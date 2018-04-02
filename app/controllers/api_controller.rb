27a43928d2b70fe26f9296d372b6c12e9f9d9261
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-01 18:14:20 -0600

    api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,7 @@ class ApiController < ActionController::API
   end
 
   def current_player
-    user_id = ApiKey.find_by(api_key: response.request.env["HTTP_X_API_KEY"]).user_id
+    user_id = ApiKey.find_by(api_key: response.request.env["HTTP_X_API_KEY"]).user.id
     if @current_game.player_1_id == user_id
       "challenger"
     elsif @current_game.player_2_id == user_id
